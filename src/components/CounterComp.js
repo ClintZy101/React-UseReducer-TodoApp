59ab0070c80644b8e535a470d4396f18f9fcbf4d
ClintZy101 <clielizek.joy@gmail.com>
2022-04-05 17:54:38 +0900

    input number type fixed

--- src/components/CounterComp.js
+++ src/components/CounterComp.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 const initialState = 0
 
@@ -6,13 +6,18 @@ function init(initialCount) {
   return {count: initialCount};
 }
 
+// function addPayload(count, payload){
+//   return {count: count  += payload}
+// }
+
 function reducer(state, action) {
   switch (action.type) {
     case 'increment':
       return {count: state.count + 1};
     case 'decrement':
       return {count: state.count - 1};
-
+    case 'incrementByPayload':
+      return {count: state.count + action.payload}
       case 'reset':
         return init(action.payload)
     default:
@@ -22,12 +27,19 @@ function reducer(state, action) {
 
 export default function CounterComp() {
   const [state, dispatch] = useReducer(reducer, initialState, init);
+  const [inputNum, setInputNum] = useState(0)
+
   return (
     <div style={{marginTop: 10}}>
       Count: {state.count}
       <button
         onClick={() => dispatch({type: 'reset', payload: initialState})}>
         Reset
+      </button>
+      <input type="number" onChange={(e)=> setInputNum(e.target.value)}/>
+      <button
+        onClick={() => dispatch({type: 'incrementByPayload', payload: parseInt(inputNum) })}>
+        Increment By Payload
       </button>
       <button onClick={() => dispatch({type: 'decrement'})}>-</button>
       <button onClick={() => dispatch({type: 'increment'})}>+</button>
